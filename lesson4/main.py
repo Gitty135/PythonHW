fd8ee0368816fcca0a7d710424645dcaf50fbc88
הנדסת תוכנה <huser@sem.local>
2025-11-06 10:44:45 +0200

    hwLesson4

---
+++ lesson4/main.py
@@ -0,0 +1,44 @@
+# 1
+def num_of_words(file):
+    count = 0
+    with open(file, 'r') as file_read:
+        for line in file_read:
+            count += 1
+            count += line.count(' ')
+    return count
+
+
+with open('file.txt', 'w') as file:
+    file.write("hello world\nhello Yehudit hhh")
+
+# print(num_of_words('file.txt'))
+
+# 2
+import csv
+
+
+def ex2(list_of_lists):
+    with open('file_user', 'w') as f:
+        writer = csv.writer(f)
+        for l in list_of_lists:
+            writer.writerow(l)
+
+
+lists = [["Hana", "Cohen", 32, "Modi'in"], ["Sara", "Cohen", 34, "Modi'in"]]
+ex2(lists)
+
+# 3
+
+import json
+
+
+def ex3(dict):
+    with open('data.json', 'w') as f:
+            json.dump(dict)
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+        print(data)
+
+
+dict = {"name": "Sara", "age": 43}
+ex3(dict)
